e41e4185cb2415b98cb742f14abbbb26f2971823
Temirlanskyy <nurgazytemirlan123@gmail.com>
2022-05-27 19:35:45 +0600

    ports

--- services/api/main.py
+++ services/api/main.py
@@ -33,10 +33,12 @@ import cv2
 
 class Database:
     def __init__(self):
-        self.client = MongoClient('mongodb://localhost:27017/')
+        self.client = MongoClient('mongodb://database:27017/')
         self.db = self.client['inGate_upravdom']
         self.check = self.db['check']
-        if self.check.count_documents({}) == 0:
+        self.gate = self.db['gate']
+        self.photo = self.db['photo']
+        if self.check.count_documents({}) == 0 or self.gate.count_documents({}) == 0:
             self.initial_populate()
 
     def insert(self, table, data):
@@ -68,12 +70,25 @@ class Database:
         return result
     
     def initial_populate(self):
-        door = {
-            "new": "0",
-            "count": "0",
-            "pay": "0"
-        }
-        self.insert(self.check, door)
+        if self.check.count_documents({}) == 0:
+            door = {
+                "new": "0",
+                "count": "0",
+                "pay": "0"
+            }
+            self.insert(self.check, door)
+        if self.gate.count_documents({}) == 0:
+            door = {
+                "gate": "1",
+                "status": "0"
+            }
+            self.insert(self.gate, door)
+            door = {
+                "gate": "2",
+                "status": "0"
+            }
+            self.insert(self.gate, door)
+
 
 DB = Database()
 
@@ -85,23 +100,65 @@ app = FastAPI(
 )   
 
 @app.get("/new", tags=['new'])
-async def doors(controller_id: str):
-    DB.check.find_one_and_update({'new': '0'},{"$set":{'new': controller_id}})
-
+async def doors():
+    DB.check.find_one_and_update({'new': '0'},{"$set":{'new': '1'}})
 
 @app.get("/count", tags=['count'])
-async def count(controller_id: str):
-    DB.check.find_one_and_update({'count': '0'},{"$set":{'count': controller_id}})
+async def count():
+    DB.check.find_one_and_update({'count': '0'},{"$set":{'count': '2'}})
 
 @app.get("/pay", tags=['pay'])
-async def paid(controller_id: str):
-    DB.check.find_one_and_update({'pay': '0'},{"$set":{'pay': controller_id}})
+async def paid():
+    DB.check.find_one_and_update({'pay': '0'},{"$set":{'pay': '2'}})
+
+@app.get("/new_back", tags=['new_back'])
+async def new_back():
+    DB.check.find_one_and_update({'new': '1'},{"$set":{'new': '0'}})
+
+@app.get("/pay_back", tags=['pay_back'])
+async def pay_back():
+    DB.check.find_one_and_update({'pay': '2'},{"$set":{'pay': '0'}})
+
+@app.get("/photo", tags=['photo'])
+async def photo(num: str):
+    ch=DB.get_one(DB.photo)
+    if ch:
+        DB.delete(DB.photo)
+    
+    door={
+        "last": num
+    }
+    DB.insert(DB.photo,door)
+
+@app.get("/get_photo", tags=['get_photo'])
+async def get_photo():
+    ch=DB.get_one(DB.photo)
+    return ch["last"]
+
 
 @app.get("/list", tags=['list'])
 async def list_():
         controllers = DB.get_one(DB.check)
         ans=controllers
-        if controllers['new']!='0': DB.check.find_one_and_update({'new': controllers['new']},{"$set":{'new':'0'}})
-        if controllers['count']!='0': DB.check.find_one_and_update({'count': controllers['count']},{"$set":{'count':'0'}})
-        if controllers['pay']!='0': DB.check.find_one_and_update({'pay': controllers['pay']},{"$set":{'pay':'0'}})
+        #if controllers['new']!='0': DB.check.find_one_and_update({'new': controllers['new']},{"$set":{'new':'0'}})
+        #if controllers['count']!='0': DB.check.find_one_and_update({'count': controllers['count']},{"$set":{'count':'0'}})
+        #if controllers['pay']!='0': DB.check.find_one_and_update({'pay': controllers['pay']},{"$set":{'pay':'0'}})
         return ans
+
+@app.get("/list_gate", tags=['list_gate'])
+async def list_gate():
+        controllers = DB.get_all(DB.gate)
+        return controllers
+
+@app.get("/status", tags=['status'])
+async def status(gate: str):
+    DB.gate.find_one_and_update({'gate': gate},{"$set":{'status': '1'}})
+
+@app.get("/ask", tags=['ask'])
+async def ask(gat: str):
+    ans=DB.gate.find_one({'gate': gat}, {'_id': False})
+    DB.gate.find_one_and_update({'gate': gat},{"$set":{'status': '0'}})
+    return ans['status']
+
+if __name__ == "__main__":
+	uvicorn.run("main:app", host="0.0.0.0", port=7788, reload=True, workers=4)
